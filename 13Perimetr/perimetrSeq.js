1a2632baabef8f4572e983656921f5b826492d25
baievv <baev.dav@gmail.com>
2022-06-24 11:25:17 +0300

    feat: implement func calc perimetr of sequence

---
+++ 13Perimetr/perimetrSeq.js
@@ -0,0 +1,9 @@
+"use strict";
+let n=+prompt("Step N is ..?");
+let a=2;
+alert(perSeq(a,n));
+function perSeq(a,n){
+  return a*n*4;
+}
+
+//What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
