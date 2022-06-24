cb485e7ee68f268cc4af004d3b18c2c9481ee3fd
baievv <baev.dav@gmail.com>
2022-06-24 11:22:49 +0300

    feat: implement func find opposite number

---
+++ 12OppoNumber/oppNumber.js
@@ -0,0 +1,9 @@
+"use strict";
+let num=+prompt("Enter the number");
+alert(oppNumber(num));
+
+function oppNumber(num){
+  return -num;
+}
+
+//Very simple, given an integer or a floating-point number, find its opposite.
